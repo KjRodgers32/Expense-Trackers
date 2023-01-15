6b749d0fb3ea8def0a134640308aed5b0b5f9e4f
Kevin <beast.rodgers@icloud.com>
2023-01-15 16:59:02 -0500

    cleaning up code in App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ const App = () => {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 25),
+      date: new Date(2020, 7, 25)
     },
     { id: "e2", title: "New TV", amount: 799.59, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28)
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2021, 5, 12)
     },
   ];
   return (
