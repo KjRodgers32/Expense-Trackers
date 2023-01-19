0dbb32e977148a8ae9021367db0fb1590b26ba77
Kevin <beast.rodgers@icloud.com>
2023-01-19 16:23:47 -0500

    added changes to funcitons in ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,19 +8,41 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-
+    // The commented out approach is used when you want to put multiple used states into one
+    // useState. You have to pass in a function to the setUserInput function in order to 
+    // persever the previous state of the other useState variables
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
     const titleChangeHanderer = (event) => {
         setEnteredTitle(event.target.value);
-    }
-
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         enteredTitle: event.target.value
+        //     };
+        // });
+    };
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
-    }
-
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         enteredAmount: event.target.value
+        //     };
+        // });
+    };
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
-    }
-
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState,
+        //         enteredDate: event.target.value
+        //     };
+        // });
+    };
     return (
         <form>
             <div className='new-expense__controls'>
